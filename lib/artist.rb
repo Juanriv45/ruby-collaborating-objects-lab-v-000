271b9530fe4d71057c6036925f8573946413ab51
Juan Rivera <juanriv45@gmail.com>
2018-02-28 03:51:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,8 @@ class Artist
   def self.find_or_create_by_name(name)
     if !self.find(name) then
       artist = Artist.new(artist_new).save
-      artist
+    else 
+      self.find(name)
     end
   end
   def self.find(artist)
