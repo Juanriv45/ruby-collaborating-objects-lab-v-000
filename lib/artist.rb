7175d11e90b48808c80fc024f625624a94225869
Juan Rivera <juanriv45@gmail.com>
2018-02-28 02:43:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name, :songs
-
+  @@all = []
   @songs = []
 
   def initialize(name)
@@ -8,6 +8,9 @@ class Artist
   end
   def songs
     @songs
+  end
+  def save
+    @@all << self
   end
 
 end
